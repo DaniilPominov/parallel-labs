f0f2737d7badde8c9eb6e3a6aaad7f8a1133f621
= <=>
2025-03-04 16:25:27 +0700

    with ads sorting

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -3,8 +3,8 @@
 #include <vector>
 #include <pthread.h>
 #include <unistd.h>
+#include <semaphore.h>
 #include <algorithm>
-
 // Структура для хранения информации о рекламном ролике
 struct Ad {
     int id;          // ID клиента
@@ -15,10 +15,11 @@ struct Ad {
 
 // Глобальные переменные
 std::vector<Ad> ads;            // Список всех рекламных роликов
-int total_time = 600;//86400;         // Общее время на эфире в секундах (24 часа)
+int total_time = 86400;         // Общее время на эфире в секундах 86400 = 24 часа
 int profit = 0;                 // Общая прибыль
 pthread_rwlock_t rwlock;        // Блокировка чтения-записи
 std::string filename = "ads.txt"; // Файл для хранения рекламных предложений
+sem_t read_s, write_s;
 
 // Функция для генерации случайного рекламного предложения
 Ad generate_ad(int id) {
@@ -27,23 +28,28 @@ Ad generate_ad(int id) {
 
 // Поток "писатель"
 void* writer(void* arg) {
+    srand(time(NULL));
     int id = 1;
     while (total_time>10) {
-        Ad ad = generate_ad(id++);
+
 
         // Запись в файл
+        sem_wait(&write_s); // Ожидание пока не прочитают
         pthread_rwlock_wrlock(&rwlock); // Блокировка для записи
-        std::ofstream file(filename, std::ios::app);
+        std::ofstream file(filename, std::ios::trunc);
         if (file.is_open()) {
+            for(int i = 0; i < random()%10+1; i++) {
+            Ad ad = generate_ad(id++);
             file << ad.id << " " << ad.duration << " " << ad.max_shows << " " << ad.price << "\n";
-            file.close();
             std::cout << "Писатель: Добавлено предложение от клиента " << ad.id << std::endl;
+            }
+            file.close();
         } else {
             std::cerr << "Ошибка открытия файла для записи!" << std::endl;
         }
         pthread_rwlock_unlock(&rwlock); // Разблокировка
-
-        sleep(2); // Имитация задержки
+        sem_post(&read_s); // Освобождение на чтение
+        //sleep(2); // Имитация задержки
     }
     return nullptr;
 }
@@ -51,7 +57,8 @@ void* writer(void* arg) {
 // Поток "читатель"
 void* reader(void* arg) {
     while (total_time>10) {
-        pthread_rwlock_wrlock(&rwlock); // Блокировка для чтения и записи
+        sem_wait(&read_s); // Ожидание пока будет что прочитать
+        pthread_rwlock_rdlock(&rwlock); // Блокировка для чтения и записи
         std::ifstream file(filename);
         std::vector<Ad> new_ads;
         if (file.is_open()) {
@@ -61,15 +68,18 @@ void* reader(void* arg) {
             }
             file.close();
 
-            // Очистка файла после чтения
-            std::ofstream clear_file(filename, std::ios::trunc);
-            clear_file.close();
         } else {
             std::cerr << "Ошибка открытия файла для чтения!" << std::endl;
         }
         pthread_rwlock_unlock(&rwlock); // Разблокировка
-
+        sem_post(&write_s); // Освобождение на запись
         // Обработка новых предложений
+        
+        // Сортировка по price * max_shows
+        std::sort(new_ads.begin(), new_ads.end(), [](const Ad& a, const Ad& b) {
+        return a.price * a.max_shows < b.price * b.max_shows;
+        });
+
         for (const auto& ad : new_ads) {
             if (total_time >= ad.duration * ad.max_shows) {
                 total_time -= ad.duration * ad.max_shows;
@@ -86,14 +96,16 @@ void* reader(void* arg) {
             std::cout << "Осталось времени " << total_time << "сек. " << std::endl;
         }
 
-        sleep(3); // Имитация задержки
+        //sleep(1); // Имитация задержки
     }
     return nullptr;
 }
 
 int main() {
-    // Инициализация блокировки чтения-записи
+
     pthread_rwlock_init(&rwlock, nullptr);
+    sem_init(&read_s, 0, 0);
+    sem_init(&write_s, 0, 1);
 
     // Создание потоков
     pthread_t writer_thread, reader_thread;
@@ -102,7 +114,7 @@ int main() {
 
     pthread_join(writer_thread, nullptr);
     pthread_join(reader_thread, nullptr);
-
+    std::cout << "Общая прибыль: " << profit << " денег." << std::endl;
     // Уничтожение блокировки
     pthread_rwlock_destroy(&rwlock);
 
