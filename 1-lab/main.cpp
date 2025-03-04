3e5beef5e9a9d1e1b6ad9bcebe6323ee8ce5a8b4
Daniil Pominov <53336146+DaniilPominov@users.noreply.github.com>
2025-03-04 16:57:54 +0700

    Revert "Lab5"

---
+++ 1-lab/main.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <iostream>
+#include <cstring>
+ struct ThreadData {
+    const char *text;
+    int start;        
+    int end;         
+    int vowel_count; 
+};
+
+bool is_vowel(char c) {
+    c = tolower(c);
+    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+}
+void *count_vowels(void *arg) {
+    ThreadData *data = (ThreadData *)arg;
+    data->vowel_count = 0;
+
+    for (int i = data->start; i < data->end; i++) {
+        if (is_vowel(data->text[i])) {
+            data->vowel_count++;
+        }
+    }
+
+    return NULL;
+}
+int main(){
+	int threadsCount = 6;
+	pthread_t threads[threadsCount];
+	ThreadData thread_data[threadsCount];
+	
+	std::string str;
+	std::cout << "type an english string\n";
+	std::getline(std::cin, str);
+	int textLength = str.size();
+	int chunkSize = textLength / threadsCount;
+	char text[textLength+1];
+	std::strcpy(text,str.c_str());
+		
+	for(int i = 0; i<threadsCount; i++){
+		thread_data[i].text = text;
+		thread_data[i].start = i * chunkSize;
+		thread_data[i].end = (i == threadsCount - 1) ? textLength+1 : (i + 1) * chunkSize;
+		pthread_create(&threads[i],NULL,count_vowels,&thread_data[i]);
+	}
+	for (int i = 0; i < threadsCount; i++) {
+	        pthread_join(threads[i], NULL);
+	    }
+	    int total_vowels = 0;
+	    for (int i = 0; i < threadsCount; i++) {
+	        total_vowels += thread_data[i].vowel_count;
+	    }
+	
+	    std::cout << "Total vowels in the text:\n" << total_vowels << "\n";
+	
+	    return 0;
+}
---
+++ 2-laba/main.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <vector>
+#include <pthread.h>
+#include <chrono>
+struct ThreadData {
+    int id;               
+    int m;                
+    int n;       
+    int t_count;          
+    std::vector<std::vector<double>> &A; 
+    std::vector<std::vector<double>> &B; 
+    double e;            
+	ThreadData(std::vector<std::vector<double>> &AA, std::vector<std::vector<double>> &BB) : A(AA), B(BB) {
+
+	}
+};
+
+void* iterate_matrix(void* arg) {
+    ThreadData* data = (ThreadData*)arg;
+    int id = data->id;
+    int m = data->m;
+    int n = data->n;
+    double e = data->e;
+    //диапазон 
+    int start = id * m / data->t_count;
+    int end = (id + 1) *m / data->t_count;
+    std::cout << start << " " << end << "\n";
+    bool done = false;
+while(done==false){
+    for (int i = start; i < end; i++) {
+        for (int j = 0; j < n; j++) {
+            double new_value;
+            if (i < m - 1) {
+                // new_value = (data->A[i][j]+data->A[i][j] + data->B[i][j]) / 3.0;
+                new_value = (data->A[i][j] + data->B[i][j] + data->B[i][j]) / 3.0;
+                // рассмотрим предпоследнюю строку матрицы, когда последняя строка сошлась в своим значениями в матрице B
+                // эту ситуацию можно записать как a_i = (a_i-1 +b +c)/3, где b,c для нас константы, это значение из матрицы B
+                // и значение из последней строки преобразуемой матрицы. Такая последовательность очевидно сходится к (b+c)/2.
+                // Невозможно сделать матрица А похожей на Б с любой заданной точностью, если не окажется, что (b+c)/2 уложится в эту точность.
+            } else {
+                new_value = (data->A[i][j] + data->B[i][j]) / 2.0;
+            }
+            data->A[i][j] = new_value;
+        }
+    }
+    done = true;
+    for (int i = start; i < end; i++) {
+        for (int j = 0; j < n; j++) {
+            double error = std::abs(data->A[i][j] - data->B[i][j]);
+            if(error>e){
+                done = false;
+            }
+        }
+    }
+    // if(done==true){
+    //     break;
+    // }
+}
+    return NULL;
+}
+
+int main() {
+    srand(time(NULL));
+    const int num_threads = 6;
+    int m = 4096; 
+    int n = 4096; 
+    double e = 0.1; 
+
+    std::vector<std::vector<double>> A(m, std::vector<double>(n, 0.0));
+    std::vector<std::vector<double>> B(m, std::vector<double>(n, 0.0));
+
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            A[i][j] = static_cast<double>(random());
+            B[i][j] = static_cast<double>(random());
+        }
+    }
+
+    std::cout << "Initial matrix A:" << std::endl;
+    // for (const auto& row : A) {
+    //     for (double val : row) {
+    //         std::cout << val << " ";
+    //     }
+    //     std::cout << std::endl;
+    // }
+
+    std::cout << "Initial matrix B:" << std::endl;
+    // for (const auto& row : B) {
+    //     for (double val : row) {
+    //         std::cout << val << " ";
+    //     }
+    //     std::cout << std::endl;
+    // }
+
+    auto start = std::chrono::high_resolution_clock::now();
+    pthread_t threads[num_threads];
+    std::vector<ThreadData*> thread_data;
+
+	for(int i = 0; i<num_threads; i++){
+		auto tdata = new ThreadData(A,B);
+		thread_data.push_back(tdata);
+	}
+
+        for (int i = 0; i < num_threads; i++) {
+            thread_data[i]->id = i;
+            thread_data[i]->m = m;
+            thread_data[i]->n = n;
+            thread_data[i]->e = e;
+            thread_data[i]->t_count = num_threads;
+            pthread_create(&threads[i], nullptr, iterate_matrix, thread_data[i]);
+        }
+
+        // Ожидаем завершения всех потоков
+        for (int i = 0; i < num_threads; ++i) {
+            pthread_join(threads[i], nullptr);
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    // Вывод итоговой матрицы A
+    std::cout << "Final matrix A:" << std::endl;
+    std::cout << "Время выполнения: " << duration.count() << " ms." << std::endl;
+    // for (const auto& row : A) {
+    //     for (double val : row) {
+    //         std::cout << val << "|";
+    //     }
+    //     std::cout << std::endl;
+    // }
+
+    return 0;
+}
