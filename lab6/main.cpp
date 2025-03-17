5ca6d8300d88b9cf931ebd3e2a6db3c98f2fb468
Daniil Pominov <d.pominov2016@yandex.ru>
2025-03-17 18:57:46 +0300

    не удаляются элементы из списка, но запускается

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -3,19 +3,16 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <time.h>
-
+#include <forward_list>
 // Простейший односвязный список
 class Node {
     public:
     int x, y, z;
-    Node* next;
 };
 
 // Глобальные переменные списка
-Node* list_head = NULL;
-Node* list_bottom = NULL;
+std::forward_list<Node> nodes;
 int list_length = 0;
-
 // Синхронизация
 pthread_rwlock_t list_rwlock = PTHREAD_RWLOCK_INITIALIZER;
 pthread_mutex_t cond_mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -33,21 +30,19 @@ void* adder_thread(void* arg) {
         new_node.z = rand() % 100;
 
         pthread_rwlock_wrlock(&list_rwlock);
-        new_node.next = list_head;
-        //смещаем голову
-        list_head = &new_node;
-        int new_length = ++list_length;
+
+        nodes.push_front(new_node);
+        list_length++;
         pthread_rwlock_unlock(&list_rwlock);
 
-        if (new_length == 1) {
+        if (list_length == 1) {
 
-            list_bottom = list_head;
             pthread_cond_broadcast(&cond_non_empty);
         }
-        if (new_length == 2) {
+        if (list_length == 2) {
             pthread_cond_broadcast(&cond_min_two);
         }
-        if (new_length == 3) {
+        if (list_length == 3) {
             pthread_cond_broadcast(&cond_min_three);
         }
 
@@ -60,16 +55,14 @@ void* adder_thread(void* arg) {
 void* remover_thread(void* arg) {
     while (1) {
         pthread_rwlock_wrlock(&list_rwlock);
-        while (list_head == NULL) {
+        while (!nodes.empty()) {
             pthread_rwlock_unlock(&list_rwlock);
             pthread_mutex_lock(&cond_mutex);
             pthread_cond_wait(&cond_non_empty, &cond_mutex);
             pthread_mutex_unlock(&cond_mutex);
             pthread_rwlock_wrlock(&list_rwlock);
         }
-        Node* to_remove = list_bottom;
-        list_bottom = list_bottom->next;
-        free(to_remove);
+        nodes.pop_front();
         list_length--;
         pthread_rwlock_unlock(&list_rwlock);
 
@@ -95,13 +88,25 @@ void* scalar_product_thread(void* arg) {
         int idx1 = rand() % list_length;
         int idx2 = rand() % list_length;
         while (idx2 == idx1) idx2 = rand() % list_length;
+        auto current = nodes.begin();
+        auto end = nodes.end();
+        int i = 0;
+        Node node1, node2;
 
-        Node *node1 = list_head, *node2 = list_head;
-        for (int i = 0; i < idx1; i++) node1 = node1->next;
-        for (int i = 0; i < idx2; i++) node2 = node2->next;
+        while(current!=end){
+            if(i==idx1){
+                node1 = *current;
+            }
+            else if(i==idx2){
+                node2 = *current;
+            }
+            current++;
+            i++;
+        }
 
-        int product = node1->x * node2->x + node1->y * node2->y + node1->z * node2->z;
-        printf("Scalar product: %d\n", product);
+        double product = node1.x * node2.x + node1.y * node2.y + node1.z * node2.z;
+        printf("Scalar product: %f\n", product);
+        printf("%d\n",list_length);
         pthread_rwlock_unlock(&list_rwlock);
 
         sleep(1);
@@ -126,17 +131,27 @@ void* vector_product_thread(void* arg) {
         int idx1 = rand() % list_length;
         int idx2 = rand() % list_length;
         while (idx2 == idx1) idx2 = rand() % list_length;
+        auto current = nodes.begin();
+        auto end = nodes.end();
+        int i = 0;
+        Node node1, node2;
+        
+        while(current!=end){
+            if(i==idx1){
+                node1 = *current;
+            }
+            else if(i==idx2){
+                node2 = *current;
+            }
+            current++;
+            i++;
+        }
 
-        Node *node1 = list_head, *node2 = list_head;
-        for (int i = 0; i < idx1; i++) node1 = node1->next;
-        for (int i = 0; i < idx2; i++) node2 = node2->next;
-
-        int cross_x = node1->y * node2->z - node1->z * node2->y;
-        int cross_y = node1->z * node2->x - node1->x * node2->z;
-        int cross_z = node1->x * node2->y - node1->y * node2->x;
+        double cross_x = node1.y * node2.z - node1.z * node2.y;
+        double cross_y = node1.z * node2.x - node1.x * node2.z;
+        double cross_z = node1.x * node2.y - node1.y * node2.x;
         printf("Vector product: (%d, %d, %d)\n", cross_x, cross_y, cross_z);
         pthread_rwlock_unlock(&list_rwlock);
-
         sleep(1);
     }
     return NULL;
@@ -152,20 +167,26 @@ void* coplanar_pairs_thread(void* arg) {
         pthread_mutex_unlock(&cond_mutex);
 
         pthread_rwlock_rdlock(&list_rwlock);
-        Node *curr = list_head;
-        while (curr) {
-            Node *next = curr->next;
-            while (next) {
-                int cross_x = curr->y * next->z - curr->z * next->y;
-                int cross_y = curr->z * next->x - curr->x * next->z;
-                int cross_z = curr->x * next->y - curr->y * next->x;
-                if (cross_x == 0 && cross_y == 0 && cross_z == 0) {
-                    printf("Collinear pair found: (%d,%d,%d) and (%d,%d,%d)\n",
-                           curr->x, curr->y, curr->z, next->x, next->y, next->z);
+        auto curr = nodes.begin();
+        auto end = nodes.end();
+        bool endFlag=false;
+        while (curr!=end && !endFlag) {
+            Node node1 = *curr;
+            auto curr2 = curr;
+            while (curr2!=end && !endFlag && curr!=curr2) {
+                Node node2 = *curr2;
+                double cross_x = node1.y * node2.z - node1.z * node2.y;
+                double cross_y = node1.z * node2.x - node1.x * node2.z;
+                double cross_z = node1.x * node2.y - node1.y * node2.x;
+                if (cross_x == 0.0 && cross_y == 0.0 && cross_z == 0.0) {
+                    printf("Collinear pair found: (%f,%f,%f) and (%f,%f,%f)\n",
+                        node1.x, node1.y, node1.z, node2.x, node2.y, node2.z);
+                        endFlag = true;
+                        break;
                 }
-                next = next->next;
+                curr2++;
             }
-            curr = curr->next;
+            curr++;
         }
         pthread_rwlock_unlock(&list_rwlock);
 
@@ -184,25 +205,30 @@ void* coplanar_triples_thread(void* arg) {
         pthread_mutex_unlock(&cond_mutex);
 
         pthread_rwlock_rdlock(&list_rwlock);
-        Node *a = list_head;
-        while (a) {
-            Node *b = a->next;
-            while (b) {
-                Node *c = b->next;
-                while (c) {
-                    int cross_x = b->y * c->z - b->z * c->y;
-                    int cross_y = b->z * c->x - b->x * c->z;
-                    int cross_z = b->x * c->y - b->y * c->x;
-                    int dot = a->x * cross_x + a->y * cross_y + a->z * cross_z;
-                    if (dot == 0) {
+        auto curr = nodes.begin();
+        auto end = nodes.end();
+        bool endFlag=false;
+        while (curr!=end && !endFlag) {
+            Node a = *curr;
+            auto curr2 = curr;
+            while (curr2!=end && !endFlag && curr!=curr2) {
+                Node b = *curr2;
+                auto curr3 = curr;
+                while (curr3!=end && !endFlag && curr2!=curr3) {
+                    Node c = *curr3;
+                    double cross_x = b.y * c.z - b.z * c.y;
+                    double cross_y = b.z * c.x - b.x * c.z;
+                    double cross_z = b.x * c.y - b.y * c.x;
+                    double dot = a.x * cross_x + a.y * cross_y + a.z * cross_z;
+                    if (dot == 0.0) {
                         printf("Coplanar triple found: (%d,%d,%d), (%d,%d,%d), (%d,%d,%d)\n",
-                               a->x, a->y, a->z, b->x, b->y, b->z, c->x, c->y, c->z);
+                               a.x, a.y, a.z, b.x, b.y, b.z, c.x, c.y, c.z);
                     }
-                    c = c->next;
+                    curr3++;
                 }
-                b = b->next;
+                curr2++;
             }
-            a = a->next;
+            curr++;
         }
         pthread_rwlock_unlock(&list_rwlock);
 
