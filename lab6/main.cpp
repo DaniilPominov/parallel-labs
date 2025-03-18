477769b525b5965184f5db1d9b9ddf20baadf21d
Daniil Pominov <d.pominov2016@yandex.ru>
2025-03-18 06:15:23 +0300

    установлена правильная нумерация работ

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -1,298 +1,122 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
 #include <pthread.h>
 #include <unistd.h>
-#include <time.h>
-#include <forward_list>
-#include <iostream>
-// Простейший односвязный список
-class Node {
-    public:
-    int x, y, z;
+#include <semaphore.h>
+#include <algorithm>
+// Структура для хранения информации о рекламном ролике
+struct Ad {
+    int id;          // ID клиента
+    int duration;    // Длительность ролика в секундах
+    int max_shows;   // Максимальное количество показов
+    int price;       // Цена за один показ
 };
 
-// Глобальные переменные списка
-std::forward_list<Node> nodes;
-int list_length = 0;
-// Синхронизация
-pthread_rwlock_t list_rwlock = PTHREAD_RWLOCK_INITIALIZER;
-pthread_mutex_t cond_mutex = PTHREAD_MUTEX_INITIALIZER;
+// Глобальные переменные
+int total_time = 86400;         // Общее время на эфире в секундах 86400 = 24 часа
+int profit = 0;                 // Общая прибыль
+std::string filename = "ads.txt"; // Файл для хранения рекламных предложений
 pthread_cond_t cond_non_empty = PTHREAD_COND_INITIALIZER;
-pthread_cond_t cond_min_two = PTHREAD_COND_INITIALIZER;
-pthread_cond_t cond_min_three = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t cond_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-// Поток добавления элементов
-void* adder_thread(void* arg) {
-    pthread_rwlock_wrlock(&list_rwlock);
-    for(int j=0;j<10;j++){
-        Node new_node;
+// Функция для генерации случайного рекламного предложения
+Ad generate_ad(int id) {
+    return {id, rand() % 60 + 1, rand() % 10 + 1, rand() % 100 + 50};
+}
 
-        new_node.x = rand() % 100;
-        new_node.y = rand() % 100;
-        new_node.z = rand() % 100;
+// Поток "писатель"
+void* writer(void* arg) {
+    srand(time(NULL));
+    int id = 1;
+    while (total_time>10) {
+        // Запись в файл
+        //pthread_mutex_lock(&cond_mutex); // Ожидание пока не прочитают
+        //pthread_rwlock_wrlock(&rwlock); // Блокировка для записи
+        std::ofstream file(filename, std::ios::trunc);
+        if (file.is_open()) {
+            for(int i = 0; i < random()%10+1; i++) {
+            Ad ad = generate_ad(id++);
+            file << ad.id << " " << ad.duration << " " << ad.max_shows << " " << ad.price << "\n";
+            std::cout << "Писатель: Добавлено предложение от клиента " << ad.id << std::endl;
+            }
+            file.close();
+        } else {
+            std::cerr << "Ошибка открытия файла для записи!" << std::endl;
+        }
+        pthread_cond_broadcast(&cond_non_empty);
+        //pthread_rwlock_unlock(&rwlock); // Разблокировка
+        //pthread_mutex_unlock(&cond_mutex); // Освобождение на чтение
+        //sleep(2); // Имитация задержки
+    }
+    return nullptr;
+}
 
+// Поток "читатель"
+void* reader(void* arg) {
+    while (total_time>10) {
+        pthread_mutex_lock(&cond_mutex); // Ожидание пока будет что прочитать
+        pthread_cond_wait(&cond_non_empty, &cond_mutex);
+        //pthread_rwlock_rdlock(&rwlock); // Блокировка для чтения и записи
+        std::ifstream file(filename);
+        std::vector<Ad> new_ads;
+        if (file.is_open()) {
+            int id, duration, max_shows, price;
+            while (file >> id >> duration >> max_shows >> price) {
+                new_ads.push_back({id, duration, max_shows, price});
+            }
+            file.close();
+
+        } else {
+            std::cerr << "Ошибка открытия файла для чтения!" << std::endl;
+        }
+        pthread_mutex_unlock(&cond_mutex); // Разблокировка
+        //sem_post(&write_s); // Освобождение на запись
+        // Обработка новых предложений
         
+        // Сортировка по price * max_shows / duration = monet per second (от большего к меньшему)
+        std::sort(new_ads.begin(), new_ads.end(), [](const Ad& a, const Ad& b) {
+        return (a.price * a.max_shows)/a.duration > (b.price * b.max_shows)/b.duration;
+        });
 
-        nodes.push_front(new_node);
-        list_length++;
-        
-
-        if (list_length == 1) {
-
-            pthread_cond_broadcast(&cond_non_empty);
-        }
-        if (list_length == 2) {
-            pthread_cond_broadcast(&cond_min_two);
-        }
-        if (list_length == 3) {
-            pthread_cond_broadcast(&cond_min_three);
-        }
-        std::cout<<"created new node\n";
-    }
-    pthread_rwlock_unlock(&list_rwlock);
-    while (1) {
-        Node new_node;
-
-        new_node.x = rand() % 100;
-        new_node.y = rand() % 100;
-        new_node.z = rand() % 100;
-
-        pthread_rwlock_wrlock(&list_rwlock);
-
-        nodes.push_front(new_node);
-        list_length++;
-        pthread_rwlock_unlock(&list_rwlock);
-
-        if (list_length == 1) {
-
-            pthread_cond_broadcast(&cond_non_empty);
-        }
-        if (list_length == 2) {
-            pthread_cond_broadcast(&cond_min_two);
-        }
-        if (list_length == 3) {
-            pthread_cond_broadcast(&cond_min_three);
-        }
-        std::cout<<"created new node\n";
-
-        //sleep(1);
-    }
-    return NULL;
-}
-
-// Поток удаления элементов
-void* remover_thread(void* arg) {
-    while (1) {
-        if(!nodes.empty()){
-            auto current = nodes.begin();
-            auto end = nodes.end();
-            int sub_iter = 0;
-            while(sub_iter<list_length-3){
-                sub_iter++;
-                current++;
+        for (const auto& ad : new_ads) {
+            if (total_time >= ad.duration * ad.max_shows) {
+                total_time -= ad.duration * ad.max_shows;
+                profit += ad.price * ad.max_shows;
+                std::cout << "Читатель: Ролик клиента " << ad.id << " размещен. Прибыль: " << ad.price * ad.max_shows << std::endl;
+            } else if (total_time >= ad.duration) {
+                int possible_shows = total_time / ad.duration;
+                total_time -= ad.duration * possible_shows;
+                profit += ad.price * possible_shows;
+                std::cout << "Читатель: Ролик клиента " << ad.id << " размещен частично. Показов: " << possible_shows << ", Прибыль: " << ad.price * possible_shows << std::endl;
+            } else {
+                std::cout << "Читатель: Ролик клиента " << ad.id << " не может быть размещен из-за нехватки времени." << std::endl;
             }
-            pthread_rwlock_wrlock(&list_rwlock);
+            std::cout << "Осталось времени " << total_time << "сек. " << std::endl;
+        }
 
-            nodes.erase_after(current,end);
-            list_length--;
-            pthread_rwlock_unlock(&list_rwlock);
-        }     
-        std::cout<<"remowed node\n";
-        //sleep(1);
+        //sleep(1); // Имитация задержки
     }
-    return NULL;
-}
-
-// Поток вычисления скалярного произведения
-void* scalar_product_thread(void* arg) {
-    while (1) {
-        pthread_mutex_lock(&cond_mutex);
-        while (list_length < 2) {
-            pthread_cond_wait(&cond_min_two, &cond_mutex);
-        }
-        pthread_mutex_unlock(&cond_mutex);
-
-        pthread_rwlock_rdlock(&list_rwlock);
-        if (list_length < 2) {
-            pthread_rwlock_unlock(&list_rwlock);
-            continue;
-        }
-        int idx1 = rand() % list_length;
-        int idx2 = rand() % list_length;
-        while (idx2 == idx1) idx2 = rand() % list_length;
-        auto current = nodes.begin();
-        auto end = nodes.end();
-        int i = 0;
-        Node node1, node2;
-
-        while(current!=end){
-            if(i==idx1){
-                node1 = *current;
-            }
-            else if(i==idx2){
-                node2 = *current;
-            }
-            current++;
-            i++;
-        }
-
-        double product = node1.x * node2.x + node1.y * node2.y + node1.z * node2.z;
-        printf("Scalar product: %f\n", product);
-        printf("Len of list:%d\n",list_length);
-        pthread_rwlock_unlock(&list_rwlock);
-
-        //sleep(1);
-    }
-    return NULL;
-}
-
-// Поток вычисления векторного произведения
-void* vector_product_thread(void* arg) {
-    while (1) {
-        pthread_mutex_lock(&cond_mutex);
-        while (list_length < 2) {
-            pthread_cond_wait(&cond_min_two, &cond_mutex);
-        }
-        pthread_mutex_unlock(&cond_mutex);
-
-        pthread_rwlock_rdlock(&list_rwlock);
-        if (list_length < 2) {
-            pthread_rwlock_unlock(&list_rwlock);
-            continue;
-        }
-        int idx1 = rand() % list_length;
-        int idx2 = rand() % list_length;
-        while (idx2 == idx1) idx2 = rand() % list_length;
-        auto current = nodes.begin();
-        auto end = nodes.end();
-        int i = 0;
-        Node node1, node2;
-        
-        while(current!=end){
-            if(i==idx1){
-                node1 = *current;
-            }
-            else if(i==idx2){
-                node2 = *current;
-            }
-            current++;
-            i++;
-        }
-
-        double cross_x = node1.y * node2.z - node1.z * node2.y;
-        double cross_y = node1.z * node2.x - node1.x * node2.z;
-        double cross_z = node1.x * node2.y - node1.y * node2.x;
-        printf("Vector product: (%d, %d, %d)\n", cross_x, cross_y, cross_z);
-        pthread_rwlock_unlock(&list_rwlock);
-        //sleep(1);
-    }
-    return NULL;
-}
-
-// Поток поиска компланарных пар (коллинеарных)
-void* coplanar_pairs_thread(void* arg) {
-    while (1) {
-        pthread_mutex_lock(&cond_mutex);
-        while (list_length < 2) {
-            pthread_cond_wait(&cond_min_two, &cond_mutex);
-        }
-        pthread_mutex_unlock(&cond_mutex);
-
-        pthread_rwlock_rdlock(&list_rwlock);
-        auto curr = nodes.begin();
-        auto end = nodes.end();
-        bool endFlag=false;
-        while (curr!=end && !endFlag) {
-            Node node1 = *curr;
-            auto curr2 = curr;
-            while (curr2!=end && !endFlag && curr!=curr2) {
-                Node node2 = *curr2;
-                double cross_x = node1.y * node2.z - node1.z * node2.y;
-                double cross_y = node1.z * node2.x - node1.x * node2.z;
-                double cross_z = node1.x * node2.y - node1.y * node2.x;
-                if (cross_x == 0.0 && cross_y == 0.0 && cross_z == 0.0) {
-                    printf("Collinear pair found: (%f,%f,%f) and (%f,%f,%f)\n",
-                        node1.x, node1.y, node1.z, node2.x, node2.y, node2.z);
-                        endFlag = true;
-                        break;
-                }
-                curr2++;
-            }
-            curr++;
-        }
-        pthread_rwlock_unlock(&list_rwlock);
-
-        //sleep(1);
-    }
-    return NULL;
-}
-
-// Поток поиска компланарных троек
-void* coplanar_triples_thread(void* arg) {
-    while (1) {
-        pthread_mutex_lock(&cond_mutex);
-        while (list_length < 3) {
-            pthread_cond_wait(&cond_min_three, &cond_mutex);
-        }
-        pthread_mutex_unlock(&cond_mutex);
-
-        pthread_rwlock_rdlock(&list_rwlock);
-        auto curr = nodes.begin();
-        auto end = nodes.end();
-        bool endFlag=false;
-        while (curr!=end && !endFlag) {
-            Node a = *curr;
-            auto curr2 = curr;
-            while (curr2!=end && !endFlag && curr!=curr2) {
-                Node b = *curr2;
-                auto curr3 = curr;
-                while (curr3!=end && !endFlag && curr2!=curr3) {
-                    Node c = *curr3;
-                    double cross_x = b.y * c.z - b.z * c.y;
-                    double cross_y = b.z * c.x - b.x * c.z;
-                    double cross_z = b.x * c.y - b.y * c.x;
-                    double dot = a.x * cross_x + a.y * cross_y + a.z * cross_z;
-                    if (dot == 0.0) {
-                        printf("Coplanar triple found: (%d,%d,%d), (%d,%d,%d), (%d,%d,%d)\n",
-                               a.x, a.y, a.z, b.x, b.y, b.z, c.x, c.y, c.z);
-                    }
-                    curr3++;
-                }
-                curr2++;
-            }
-            curr++;
-        }
-        pthread_rwlock_unlock(&list_rwlock);
-
-        //sleep(1);
-    }
-    return NULL;
+    return nullptr;
 }
 
 int main() {
-    srand(time(NULL));
 
-    pthread_t adder, remover, scalar, vector, coplanar_pairs, coplanar_triples;
+   // pthread_rwlock_init(&rwlock, nullptr);
+    //sem_init(&read_s, 0, 0);
+   // sem_init(&write_s, 0, 1);
 
-    pthread_create(&adder, NULL, adder_thread, NULL);
-    pthread_create(&remover, NULL, remover_thread, NULL);
-    pthread_create(&scalar, NULL, scalar_product_thread, NULL);
-    pthread_create(&vector, NULL, vector_product_thread, NULL);
-    pthread_create(&coplanar_pairs, NULL, coplanar_pairs_thread, NULL);
-    pthread_create(&coplanar_triples, NULL, coplanar_triples_thread, NULL);
+    // Создание потоков
+    pthread_t writer_thread, reader_thread;
+    pthread_create(&writer_thread, nullptr, writer, nullptr);
+    pthread_create(&reader_thread, nullptr, reader, nullptr);
 
-    pthread_join(adder, NULL);
-    pthread_join(remover, NULL);
-    pthread_join(scalar, NULL);
-    pthread_join(vector, NULL);
-    pthread_join(coplanar_pairs, NULL);
-    pthread_join(coplanar_triples, NULL);
-
-    pthread_rwlock_destroy(&list_rwlock);
+    pthread_join(writer_thread, nullptr);
+    pthread_join(reader_thread, nullptr);
+    std::cout << "Общая прибыль: " << profit << " денег." << std::endl;
+    // Уничтожение блокировки
     pthread_mutex_destroy(&cond_mutex);
     pthread_cond_destroy(&cond_non_empty);
-    pthread_cond_destroy(&cond_min_two);
-    pthread_cond_destroy(&cond_min_three);
 
     return 0;
 }
