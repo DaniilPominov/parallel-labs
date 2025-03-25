2ff58bdc30d316ec41c1f702fd73a80fb4f7f97d
DaniilPominov <d.pominov2016@yandex.ru>
2025-03-25 14:44:45 +0700

    работа защищена

--- lab7/main.cpp
+++ lab7/main.cpp
@@ -5,7 +5,7 @@
 #include <time.h>
 #include <forward_list>
 #include <iostream>
-// Простейший односвязный список
+// Простейший узел
 class Node {
     public:
     int x, y, z;
@@ -24,15 +24,17 @@ pthread_cond_t cond_erased = PTHREAD_COND_INITIALIZER;
 
 void sleep_ms(int milliseconds) {
     struct timespec ts;
-    ts.tv_sec = milliseconds / 1000;          // Секунды
-    ts.tv_nsec = (milliseconds % 1000) * 1e6;  // Наносекунды (1 мс = 1 000 000 нс)
+    ts.tv_sec = milliseconds / 1000;
+    ts.tv_nsec = (milliseconds % 1000) * 1e6;  
 
-    nanosleep(&ts, NULL);  // Задержка с высокой точностью
+    nanosleep(&ts, NULL);  
 }
 
 // Поток добавления элементов
 void* adder_thread(void* arg) {
     pthread_rwlock_wrlock(&list_rwlock);
+    //поток делетер запускается последним поэтому это
+    //холостой прогон, чтобы в список был не пуст изначально
     for(int i=0; i<10; i++){
         Node new_node;
         new_node.x = rand() % 100;
@@ -60,6 +62,8 @@ void* adder_thread(void* arg) {
     pthread_rwlock_unlock(&list_rwlock);
 
     while (1) {
+        //пока старое не удалится- новое не пишем
+        pthread_mutex_lock(&cond_mutex);
         pthread_cond_wait(&cond_erased, &cond_mutex);
         pthread_rwlock_wrlock(&list_rwlock);
         for(int i=0; i<10; i++){
@@ -85,9 +89,11 @@ void* adder_thread(void* arg) {
             pthread_cond_broadcast(&cond_min_three);
         }
         std::cout<<"created new node\n";
+        
     }
+    pthread_mutex_unlock(&cond_mutex);
     pthread_rwlock_unlock(&list_rwlock);
-        //sleep(1);
+        
     }
     return NULL;
 }
@@ -95,7 +101,8 @@ void* adder_thread(void* arg) {
 // Поток удаления элементов
 void* remover_thread(void* arg) {
     while (1) {
-        if(list_length>1){
+        if(list_length>0){
+            pthread_mutex_lock(&cond_mutex);
             auto current = nodes.begin();
             auto end = nodes.end();
             int sub_iter = 0;
@@ -110,10 +117,11 @@ void* remover_thread(void* arg) {
             list_length-=sub_iter;
             std::cout<<"remowed nodes\n";
             pthread_rwlock_unlock(&list_rwlock);
+            pthread_mutex_unlock(&cond_mutex);
             pthread_cond_signal(&cond_erased);
         }     
+        
         sleep_ms(100);
-        //sleep(1);
     }
     return NULL;
 }
@@ -126,7 +134,8 @@ void* scalar_product_thread(void* arg) {
         while (list_length < 2) {
             pthread_cond_wait(&cond_min_two, &cond_mutex);
         }
-        
+        pthread_mutex_unlock(&cond_mutex);
+
         pthread_rwlock_rdlock(&list_rwlock);
         int idx1 = rand() % list_length;
         int idx2 = rand() % list_length;
@@ -149,7 +158,7 @@ void* scalar_product_thread(void* arg) {
 
         double product = node1.x * node2.x + node1.y * node2.y + node1.z * node2.z;
         printf("Scalar product: %f\n", product);
-        pthread_mutex_unlock(&cond_mutex);
+        
         pthread_rwlock_unlock(&list_rwlock);
 
         //sleep(1);
@@ -164,7 +173,7 @@ void* vector_product_thread(void* arg) {
         while (list_length < 2) {
             pthread_cond_wait(&cond_min_two, &cond_mutex);
         }
-        
+        pthread_mutex_unlock(&cond_mutex);
         pthread_rwlock_rdlock(&list_rwlock);
 
         int idx1 = rand() % list_length;
@@ -191,7 +200,7 @@ void* vector_product_thread(void* arg) {
         double cross_z = node1.x * node2.y - node1.y * node2.x;
         printf("Vector product: (%d, %d, %d)\n", cross_x, cross_y, cross_z);
         pthread_rwlock_unlock(&list_rwlock);
-        pthread_mutex_unlock(&cond_mutex);
+        
         //sleep(1);
     }
     return NULL;
@@ -204,6 +213,7 @@ void* coplanar_pairs_thread(void* arg) {
         while (list_length < 2) {
             pthread_cond_wait(&cond_min_two, &cond_mutex);
         }
+        pthread_mutex_unlock(&cond_mutex);
         
         pthread_rwlock_rdlock(&list_rwlock);
         auto curr = nodes.begin();
@@ -230,7 +240,6 @@ void* coplanar_pairs_thread(void* arg) {
             curr++;
         }
         pthread_rwlock_unlock(&list_rwlock);
-        pthread_mutex_unlock(&cond_mutex);
 
         //sleep(1);
     }
@@ -244,7 +253,7 @@ void* coplanar_triples_thread(void* arg) {
         while (list_length < 3) {
             pthread_cond_wait(&cond_min_three, &cond_mutex);
         }
-        
+        pthread_mutex_unlock(&cond_mutex);
 
         pthread_rwlock_rdlock(&list_rwlock);
         auto curr = nodes.begin();
@@ -264,8 +273,8 @@ void* coplanar_triples_thread(void* arg) {
                     double cross_z = b.x * c.y - b.y * c.x;
                     double dot = a.x * cross_x + a.y * cross_y + a.z * cross_z;
                     if (dot == 0.0) {
-                        printf("Coplanar triple found: (%d,%d,%d), (%d,%d,%d), (%d,%d,%d)\n",
-                               a.x, a.y, a.z, b.x, b.y, b.z, c.x, c.y, c.z);
+                         printf("Coplanar triple found: (%d,%d,%d), (%d,%d,%d), (%d,%d,%d)\n",
+                                a.x, a.y, a.z, b.x, b.y, b.z, c.x, c.y, c.z);
                                endFlag = true;
                                break;
                     }
@@ -276,7 +285,7 @@ void* coplanar_triples_thread(void* arg) {
             }
             curr++;
         }
-        pthread_mutex_unlock(&cond_mutex);
+        
         pthread_rwlock_unlock(&list_rwlock);
 
         //sleep(1);
@@ -290,11 +299,11 @@ int main() {
     pthread_t adder, remover, scalar, vector, coplanar_pairs, coplanar_triples;
 
     pthread_create(&adder, NULL, adder_thread, NULL);
-    pthread_create(&remover, NULL, remover_thread, NULL);
     pthread_create(&scalar, NULL, scalar_product_thread, NULL);
     pthread_create(&vector, NULL, vector_product_thread, NULL);
     pthread_create(&coplanar_pairs, NULL, coplanar_pairs_thread, NULL);
     pthread_create(&coplanar_triples, NULL, coplanar_triples_thread, NULL);
+    pthread_create(&remover, NULL, remover_thread, NULL);
 
     pthread_join(adder, NULL);
     pthread_join(remover, NULL);
